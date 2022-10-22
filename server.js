c3a78c8049e00eee122ca21e7d0514f9e7524a72
profejean <profesorjeandiaz@gmail.com>
2022-10-22 06:33:29 -0400

    other change serverjs

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static('./dist/'));
+app.use(express.static(`${__dirname}/net-grid-frontend/dist/`));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+app.get('*', (req, res) => {
+    res.sendFile(`./net-grid-frontend/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
+
 app.listen(process.env.PORT || 8080);
 
