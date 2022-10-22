a48d9f4af6e3adf284a22200ab2b77e367db7e67
profejean <profesorjeandiaz@gmail.com>
2022-10-22 06:39:56 -0400

    erver js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static('./dist/'));
-app.use(express.static(`${__dirname}/net-grid-frontend/dist/`));
 
-app.get('*', (req, res) => {
-    res.sendFile(`./net-grid-frontend/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/net-grid-frontend'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname,'/dist/net-grid-frontend/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
